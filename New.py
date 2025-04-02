969d3b06a2cf7eccd39c60a5d8e45ebdc8b4fe85
sangarssm <46450518+sangarssm@users.noreply.github.com>
2025-04-01 23:12:10 -0400

    Create New.py

---
+++ New.py
@@ -0,0 +1,52 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+df = pd.read_csv('./data/datasets(Sheet2) (2).csv')
+app = Dash()
+server = app.server
+@server.before_first_request
+def bfr():
+    print("Testing")
+app.layout = html.Div([
+    html.H1(children='FIFA Winners', style={'textAlign':'center'}),
+    html.H2("Select a Country:"),
+    dcc.RadioItems(
+        id='Countrys',
+        options=[{'label':countrys, 'value':countrys} for countrys in df['Countrys'].unique()],
+        value='Ireland',
+        inline=True,
+    ),
+ 
+    dcc.Graph(id='graph-content'),
+#])
+    html.Div(id='year-output', style={'textAlign':'center','marginTop':'20px'}),
+])
+@callback(
+    Output('graph-content', 'figure'),
+    Input('Countrys', 'value')
+)
+def update_graph(value):
+  
+
+    dff = df[df.Countrys==value]
+    
+
+    #return px.choropleth(dff, y='Wins')
+    fig = px.choropleth(
+        dff,
+        locations='Countrys',
+        #featureidkey='properties.Countrys',
+        color='Wins',
+        color_continuous_scale="Viridis", 
+        title=f'FIFA Winners',
+        labels={'Wins':'Number of Wins'},  
+        locationmode='country names',
+        hover_name='Countrys'
+
+    )
+    fig.update_geos(showcoastlines=True, coastlinecolor="Black",projection_type="natural earth")
+    return fig
+
+        
+if __name__ == '__main__':
+    app.run_server(debug=True)
